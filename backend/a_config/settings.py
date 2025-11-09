18417a12cd9cb5a17896beb1107aa5516dbb58bf
ventuno-21 <yasna.pakdel@outlook.com>
2025-11-09 08:19:00 -0800

    Using PostgreSQL as a database in project

--- backend/a_config/settings.py
+++ backend/a_config/settings.py
@@ -94,11 +94,14 @@ WSGI_APPLICATION = "a_config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_NAME"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": int(os.getenv("POSTGRES_PORT")),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
